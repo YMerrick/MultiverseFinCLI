01e9d347cf3b0d004bb429446ee1eaa567951c19
York <yorkmerrick@outlook.com>
2025-10-09 16:48:11 +0100

    try new menu implementation

--- src/main/java/com/fincore/app/Main.java
+++ src/main/java/com/fincore/app/Main.java
@@ -1,9 +1,31 @@
 package com.fincore.app;
 
+import com.fincore.app.controller.CommandHandler;
+import com.fincore.app.model.Account;
+import com.fincore.app.factory.CommandFactory;
+import com.fincore.app.menu.CLIMenuGroup;
+import com.fincore.app.menu.CLIMenuItem;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Account user = new Account(10_000);
+        CommandFactory factory = new CommandFactory(user);
+        CommandHandler controller = new CommandHandler(factory);
+
+        CLIMenuGroup mainMenu = new CLIMenuGroup("Main Menu");
+        mainMenu.addMenuItem(new CLIMenuItem("Deposit", controller::handleDeposit));
+        mainMenu.addMenuItem(new CLIMenuItem("Withdraw", controller::handleWithdraw));
+
+        CLIMenuGroup accountMenu = new CLIMenuGroup("Accounts");
+        accountMenu.addMenuItem(new CLIMenuItem("Check Balance", ));
+
+        mainMenu.addMenuItem(accountMenu);
+
+        mainMenu.select();
+
         Scanner sysInScanObj = new Scanner(System.in);
         int menuChoice;
         double accountBalance = 10000.0;
