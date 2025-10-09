97fc38dc7f490a6b0d5e5a997ddcc6ac48f6040b
York <yorkmerrick@outlook.com>
2025-10-09 22:02:56 +0100

    add boilerplate for menu controller

--- src/main/java/com/fincore/app/menu/CLIMenuComponent.java
+++ src/main/java/com/fincore/app/menu/CLIMenuComponent.java
@@ -1,6 +1,6 @@
 package com.fincore.app.menu;
 
 public interface CLIMenuComponent {
-    public String display();
+    public String render();
     public MenuDirective select();
 }
--- src/main/java/com/fincore/app/menu/CLIMenuGroup.java
+++ src/main/java/com/fincore/app/menu/CLIMenuGroup.java
@@ -1,33 +1,50 @@
 package com.fincore.app.menu;
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class CLIMenuGroup implements CLIMenuComponent {
     private final String label;
-    private final ArrayList<CLIMenuComponent> menuItemList;
+    private final List<CLIMenuComponent> menuItemList;
+    private final boolean enabled;
+
+    public CLIMenuGroup(String label, ArrayList<CLIMenuComponent> menuItems, boolean isEnabled) {
+        this.label = label;
+        this.menuItemList = new ArrayList<>(menuItems);
+        this.enabled = isEnabled;
+    }
+
+    public CLIMenuGroup(String label, boolean isEnabled) {
+        this.label = label;
+        this.enabled = isEnabled;
+        this.menuItemList = new ArrayList<CLIMenuComponent>();
+    }
 
     public CLIMenuGroup(String label, ArrayList<CLIMenuComponent> menuItems) {
         this.label = label;
-        this.menuItemList = menuItems;
+        this.enabled = true;
+        this.menuItemList = new ArrayList<>(menuItems);
     }
 
     public CLIMenuGroup(String label) {
         this.label = label;
         this.menuItemList = new ArrayList<CLIMenuComponent>();
+        this.enabled = true;
     }
 
     // Runs the submenu
     @Override
     public MenuDirective select() {
-        for (CLIMenuComponent item : menuItemList) {
-            item.display();
-        }
         return MenuDirective.GOTO_CHILD;
     }
 
     @Override
-    public String display() {
+    public String render() {
         return label;
+    }
+
+    public CLIMenuComponent getChild(int index) {
+        return menuItemList.get(index);
     }
 
     public void addMenuItem(CLIMenuComponent item) {
@@ -38,7 +55,10 @@ public class CLIMenuGroup implements CLIMenuComponent {
         menuItemList.remove(item);
     }
 
-    public void removeMenuItem(int index) {
+    public void removeMenuItem(int index) throws ArrayIndexOutOfBoundsException {
+        if (index < 0 || index >= menuItemList.size()) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
         menuItemList.remove(index);
     }
 }
