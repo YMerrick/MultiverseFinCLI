2e81711e3bc346d4ebfb5599c44995ea1ecf51d8
York <yorkmerrick@outlook.com>
2025-10-09 16:34:27 +0100

    refactor menu to be extensible

---
+++ src/main/java/com/fincore/app/menu/CLIMenuItem.java
@@ -0,0 +1,21 @@
+package com.fincore.app.menu;
+
+public class CLIMenuItem implements CLIMenuComponent {
+    private final String label;
+    private final Runnable action;
+
+    public CLIMenuItem(String label, Runnable action) {
+        this.label = label;
+        this.action = action;
+    }
+
+    @Override
+    public void display() {
+        System.out.println(label);
+    }
+
+    @Override
+    public void select() {
+        action.run();
+    }
+}
