0ea24dde6af17bde7ea4788e674b45324a95211d
York <yorkmerrick@outlook.com>
2025-10-09 22:09:42 +0100

    add input validation

--- src/main/java/com/fincore/app/controller/MenuController.java
+++ src/main/java/com/fincore/app/controller/MenuController.java
@@ -19,12 +19,31 @@ public class MenuController {
         Scanner sysIn = new Scanner(System.in);
         // Menu Loop
         for (;;) {
-
+            
         }
     }
 
     public int getInput(Scanner inStream, int menuSize) {
-        return 1;
+        int userInput = -1;
+        boolean isValid = false;
+        
+        while (!isValid) {
+            if (!inStream.hasNextInt()) {
+                System.out.print("Please enter a valid option: ");
+                inStream.next();
+                continue;
+            }
+            
+            userInput = inStream.nextInt();
+            if (userInput < 1 || userInput > menuSize) {
+                System.out.print("Please enter a valid option: ");
+                inStream.next();
+                continue;
+            }
+            
+            isValid = true;
+        }
+        return userInput;
     }
 
     public void renderMenu() {
