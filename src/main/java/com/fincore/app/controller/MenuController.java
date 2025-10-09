184562d61e387d64217bb04c53a3f79b63e9e369
York <yorkmerrick@outlook.com>
2025-10-10 00:56:32 +0100

    add menu OOP logic

--- src/main/java/com/fincore/app/controller/MenuController.java
+++ src/main/java/com/fincore/app/controller/MenuController.java
@@ -2,24 +2,40 @@ package com.fincore.app.controller;
 
 import com.fincore.app.menu.CLIMenuComponent;
 import com.fincore.app.menu.CLIMenuGroup;
+import com.fincore.app.menu.MenuDirective;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MenuController {
-    private final Deque<CLIMenuGroup> menuStack;
+    private final Deque<CLIMenuGroup> menuStack = new ArrayDeque<CLIMenuGroup>();;
 
     public MenuController(CLIMenuGroup root) {
-        this.menuStack = new ArrayDeque<CLIMenuGroup>();
         menuStack.push(root);
     }
 
     public void start() {
         Scanner sysIn = new Scanner(System.in);
-        // Menu Loop
+        CLIMenuGroup currentMenu;
+        int userInput;
+        MenuDirective directive;
+        int stackSize;
+
         for (;;) {
-            
+            currentMenu = menuStack.peek();
+            stackSize = menuStack.size();
+            if (Objects.isNull(currentMenu)) throw new NullPointerException("Menu does not exist");
+
+            renderMenu(currentMenu);
+            userInput = getInput(sysIn, currentMenu.getMenuSize());
+            directive = interpretCommandOrExit(userInput, stackSize, currentMenu);
+            switch (directive) {
+                case EXIT -> System.exit(0);
+                case BACK -> menuStack.pop();
+                case GOTO_CHILD, STAY -> {}
+            }
+
         }
     }
 
@@ -35,7 +51,7 @@ public class MenuController {
             }
             
             userInput = inStream.nextInt();
-            if (userInput < 1 || userInput > menuSize) {
+            if (userInput < 0 || userInput > menuSize) {
                 System.out.print("Please enter a valid option: ");
                 inStream.next();
                 continue;
@@ -46,15 +62,36 @@ public class MenuController {
         return userInput;
     }
 
-    public void renderMenu() {
-        CLIMenuComponent currentMenu = menuStack.peek();
-
+    public void renderMenu(CLIMenuGroup currentMenu) {
         // Get all options
+        String breadcrumb = menuStack.stream().map(CLIMenuGroup::render).collect(Collectors.joining(" > "));
+        String outputBody;
 
-        // Compose the out string
+        System.out.println(breadcrumb);
+
+        ArrayList<CLIMenuComponent> menuList = currentMenu.getChildren();
+
+        outputBody = IntStream.range(0, currentMenu.getMenuSize())
+                .mapToObj(i -> i + ". " + menuList.get(i).render())
+                .collect(Collectors.joining("\n"));
+
+        System.out.println(outputBody);
+        // Compose the output string
     }
 
-    public void callChild(int index) {
+    public MenuDirective interpretCommandOrExit(int choice, int stackSize, CLIMenuGroup currentMenu) {
+        if (choice == 0) {
+            if (stackSize > 1) return MenuDirective.EXIT;
+            else return MenuDirective.EXIT;
+        }
+        return callChild(choice, currentMenu);
+    }
+
+    public MenuDirective callChild(int index, CLIMenuGroup currentMenu) {
         // Add menu component on to the stack
+        CLIMenuComponent child = currentMenu.getChild(index);
+        if (child.isGroup()) menuStack.push((CLIMenuGroup) child);
+
+        return child.select();
     }
 }
