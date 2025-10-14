dd103e370b1dc41cccc5212f4bc5c91b93c6b465
York <yorkmerrick@outlook.com>
2025-10-14 13:26:16 +0100

    add test suite

---
+++ src/test/java/com/fincore/app/TestRunner.java
@@ -0,0 +1,11 @@
+package com.fincore.app;
+
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.fincore.app")
+public class TestRunner {
+    public static void main(String[] args) {}
+}
