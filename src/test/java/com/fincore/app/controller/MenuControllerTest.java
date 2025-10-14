db3f15bb3a29d5a3b375e41c9bd271024b36e267
York <yorkmerrick@outlook.com>
2025-10-14 11:45:05 +0100

    add placeholder tests

---
+++ src/test/java/com/fincore/app/controller/MenuControllerTest.java
@@ -0,0 +1,23 @@
+package com.fincore.app.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.fincore.app.controller.MenuController;
+import org.junit.jupiter.api.extension.ExtendWith;
+//import
+
+//@ExtendWith(Moc)
+public class MenuControllerTest {
+//    private final MenuController menuRunner = new MenuController();
+
+    @Test
+    void renderBackOrExit() {
+        // Arrange
+
+        // Act
+
+        // Assert
+//        assertEquals();
+    }
+}
