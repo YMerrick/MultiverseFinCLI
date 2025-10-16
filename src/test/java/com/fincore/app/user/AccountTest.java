a0d04a55936778ec69535fd5863144cbe1d81884
York <yorkmerrick@outlook.com>
2025-10-16 10:58:53 +0100

    add test and validation logic

--- src/test/java/com/fincore/app/user/AccountTest.java
+++ src/test/java/com/fincore/app/user/AccountTest.java
@@ -1,4 +1,75 @@
 package com.fincore.app.user;
 
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AccountTest {
+
+    static final String EXCEPTION_EXPECTED = "Illegal argument exception expected";
+
+    @Test
+    public void testDeposit() {
+        Account stubAccount = new Account();
+        String previousState = stubAccount.getBalance();
+
+        stubAccount.deposit(1);
+        String result = stubAccount.getBalance();
+
+        assertNotEquals(previousState, result);
+        assertEquals("£0.01", result);
+    }
+
+    @Test
+    public void testNegativeDeposit() {
+        Account stubAccount = new Account();
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    stubAccount.deposit(-1);
+                },
+                EXCEPTION_EXPECTED
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    stubAccount.deposit(BigDecimal.valueOf(-1.1));
+                },
+                EXCEPTION_EXPECTED
+        );
+    }
+
+    @Test
+    public void testWithdraw() {
+        Account stubAccount = new Account(2.5);
+        String previousState = stubAccount.getBalance();
+
+        stubAccount.withdraw(2.0);
+        String result = stubAccount.getBalance();
+
+        assertNotEquals(previousState, result);
+        assertEquals("£0.50", result);
+    }
+
+    @Test
+    public void testNegativeWithdraw() {
+        Account stub = new Account();
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    stub.withdraw(-1);
+                },
+                EXCEPTION_EXPECTED
+        );
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    stub.withdraw(BigDecimal.valueOf(-1.1));
+                },
+                EXCEPTION_EXPECTED
+        );
+    }
 }
--- src/test/java/com/fincore/app/user/MoneyTest.java
+++ src/test/java/com/fincore/app/user/MoneyTest.java
@@ -2,11 +2,20 @@ package com.fincore.app.user;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Currency;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
 
     @Test
-    void get() {
+    void testGet() {
+        Money stubMoney = new Money(1000, Currency.getInstance("GBP"));
+        String expected = "£10.00";
+        assertEquals(expected, stubMoney.get());
+
+        stubMoney.setAmount(0);
+        expected = "£0.00";
+        assertEquals(expected, stubMoney.get());
     }
 }
