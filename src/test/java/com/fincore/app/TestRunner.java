7e5d5ead7b4701d5edd759aa423b626bd5fbacdb
York <yorkmerrick@outlook.com>
2025-10-16 15:45:21 +0100

    fix test suite cyclical calling

--- src/test/java/com/fincore/app/TestRunner.java
+++ src/test/java/com/fincore/app/TestRunner.java
@@ -5,7 +5,10 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectPackages("com.fincore.app")
+@SelectPackages({
+        "com.fincore.app.menu",
+        "com.fincore.app.user",
+        "com.fincore.app.command",
+})
 public class TestRunner {
-    public static void main(String[] args) {}
 }
