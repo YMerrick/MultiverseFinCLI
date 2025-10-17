e38733a97b44bd27624764b3bdb3b0bc326c811a
YXMerrick <yorkmerrick@outlook.com>
2025-10-17 23:29:34 +0100

    Revert "add test logic"
    This reverts commit a42dda0cb7b5aec7115abf5f85b2c63dea33dc70.

--- src/test/java/com/fincore/app/menu/CLIMenuItemTest.java
+++ src/test/java/com/fincore/app/menu/CLIMenuItemTest.java
@@ -1,23 +1,5 @@
 package com.fincore.app.menu;
 
-import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.mock;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class CLIMenuItemTest {
-    String stubLabel = "test";
-    boolean actionFlag = false;
-    CLIMenuItem menuItem = new CLIMenuItem(stubLabel, this::stubAction);
-
-    @Test
-    public void testSelect() {
-        MenuDirective result = menuItem.select();
-        assertEquals(MenuDirective.STAY, result);
-        assertTrue(actionFlag);
-    }
-
-    private void stubAction() {
-        actionFlag = true;
-    }
+    
 }
