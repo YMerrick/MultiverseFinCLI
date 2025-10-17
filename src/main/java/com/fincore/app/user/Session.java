0bb7cf07ed089e1c1a63021bac1cab5dec3f2e98
York <yorkmerrick@outlook.com>
2025-10-17 14:41:01 +0100

    add placeholder session

--- src/main/java/com/fincore/app/user/Session.java
+++ src/main/java/com/fincore/app/user/Session.java
@@ -3,6 +3,14 @@ package com.fincore.app.user;
 import java.util.UUID;
 
 public class Session {
-    private UUID sessionId;
-    private Account user;
+    private final UUID sessionId;
+    private final AccountId userId;
+    // private roles field
+
+    public Session(AccountId user, UUID sessionId) {
+        this.sessionId = sessionId;
+        this.userId = user;
+    }
+
+
 }
