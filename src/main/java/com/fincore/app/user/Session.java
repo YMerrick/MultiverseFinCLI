ca874c7ac8ce3d42e29adab0e06537f311f73c43
YXMerrick <yorkmerrick@outlook.com>
2025-10-17 23:29:34 +0100

    Revert "add placeholder session"
    This reverts commit a8008b2b2851dc0560007c6afc8c8d09976764a3.

--- src/main/java/com/fincore/app/user/Session.java
+++ src/main/java/com/fincore/app/user/Session.java
@@ -3,14 +3,6 @@ package com.fincore.app.user;
 import java.util.UUID;
 
 public class Session {
-    private final UUID sessionId;
-    private final AccountId userId;
-    // private roles field
-
-    public Session(AccountId user, UUID sessionId) {
-        this.sessionId = sessionId;
-        this.userId = user;
-    }
-
-
+    private UUID sessionId;
+    private Account user;
 }
