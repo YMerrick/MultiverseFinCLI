9e7ffcdfe62f6187088617d521cfd66d45fd2013
YXMerrick <yorkmerrick@outlook.com>
2025-10-17 23:29:34 +0100

    Revert "fix test suite cyclical calling"
    This reverts commit ee7e9d798af3bf5be76b00b9e972a697a26e9b9f.

--- src/test/java/com/fincore/app/TestRunner.java
+++ src/test/java/com/fincore/app/TestRunner.java
@@ -5,10 +5,7 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectPackages({
-        "com.fincore.app.menu",
-        "com.fincore.app.user",
-        "com.fincore.app.command",
-})
+@SelectPackages("com.fincore.app")
 public class TestRunner {
+    public static void main(String[] args) {}
 }
