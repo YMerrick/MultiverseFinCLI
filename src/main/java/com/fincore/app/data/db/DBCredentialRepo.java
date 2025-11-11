6c1f2e3c2a7b638d8a83c0d2baf5d1f9324ca3af
York <yorkmerrick@outlook.com>
2025-11-11 17:12:56 +0000

    add credential db logic

--- src/main/java/com/fincore/app/data/db/DBCredentialRepo.java
+++ src/main/java/com/fincore/app/data/db/DBCredentialRepo.java
@@ -2,28 +2,41 @@ package com.fincore.app.data.db;
 
 import com.fincore.app.domain.identity.CredentialRepo;
 import com.fincore.app.domain.identity.Credentials;
+import lombok.AllArgsConstructor;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
+import java.util.UUID;
 
+@AllArgsConstructor
 public class DBCredentialRepo implements CredentialRepo {
     private final String url;
     private final String tableName;
 
-    public DBCredentialRepo(String url, String tableName) {
-        this.url = url;
-        this.tableName = tableName;
-    }
     @Override
     public Optional<Credentials> getByUsername(String username) {
+        String sql = String.format("""
+                SELECT * FROM %s
+                WHERE username = ?""", tableName);
+        ResultSet response;
+        Credentials credentials;
         try (Connection conn = DriverManager.getConnection(url)) {
-            String sql = """
-                    """;
+            var pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, username);
+            response = pstmt.executeQuery();
+            credentials = new Credentials(
+                    response.getString("username"),
+                    response.getString("passwordHash"),
+                    UUID.fromString(response.getString("userId"))
+            );
 
-        } catch (SQLException ignored) {}
-        return Optional.empty();
+        } catch (SQLException e) {
+            credentials = null;
+        }
+        return Optional.ofNullable(credentials);
     }
 
     @Override
@@ -33,11 +46,11 @@ public class DBCredentialRepo implements CredentialRepo {
                     INSERT INTO %s
                     VALUES (?, ?, ?)""",
                     tableName);
-            var stmt = conn.prepareStatement(sql);
+            var stmt = conn.prepareStatement(sql, new String[]{"userId, username, passwordHash"});
             stmt.setString(1, cred.userId().toString());
             stmt.setString(2, cred.username());
             stmt.setString(3, cred.passwordHash());
-            stmt.execute(sql, new String[]{"userId, username, passwordHash"});
+            stmt.executeUpdate();
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
