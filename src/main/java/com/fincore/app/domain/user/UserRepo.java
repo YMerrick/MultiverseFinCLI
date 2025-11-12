f98eea8ddd0bb41f4c1536ce0c744bb1f8250f34
York <yorkmerrick@outlook.com>
2025-11-12 13:09:09 +0000

    chore clean up unused method

--- src/main/java/com/fincore/app/domain/user/UserRepo.java
+++ src/main/java/com/fincore/app/domain/user/UserRepo.java
@@ -5,6 +5,5 @@ import java.util.UUID;
 
 public interface UserRepo {
     Optional<User> getById(UUID userId);
-    Optional<User> getByFullName(String firstName, String lastName);
     void save(User user);
 }
