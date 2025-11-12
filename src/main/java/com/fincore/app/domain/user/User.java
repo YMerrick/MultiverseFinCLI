05e3405604423a5f0e65732958eb2d9c65d779d7
York <yorkmerrick@outlook.com>
2025-11-12 13:09:32 +0000

    refactor user for dependency injection

--- src/main/java/com/fincore/app/domain/user/User.java
+++ src/main/java/com/fincore/app/domain/user/User.java
@@ -3,7 +3,7 @@ package com.fincore.app.domain.user;
 import java.util.UUID;
 
 public record User(UUID userId, String firstName, String lastName) {
-    public static User createUser( String firstName, String lastName) {
-        return new User(UUID.randomUUID(), firstName, lastName);
+    public static User createUser(UUID userId, String firstName, String lastName) {
+        return new User(userId, firstName, lastName);
     }
 }
